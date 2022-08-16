55830ebf6fe9de916996f9482fbb9ccd176f7cb5
Will Shanks <shanks@ucar.edu>
2022-08-16 10:32:48 -0600

    refactored to use iterators for linked lists

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,35 @@
 use std::ffi;
 
+use linked_list::LinkedList;
+mod linked_list;
 mod bindings;
 
-fn main() {
-    unsafe{
+linked_list::impl_LlItem!{[bindings::attrl, bindings::batch_status]}
+
+fn get_nodes() -> LinkedList<bindings::batch_status> {
+    LinkedList::new( unsafe {
         let conn = bindings::pbs_connect(0 as *mut i8);
         // second arg is null to get all nodes, third is null to get all attributes, forth is unused
-        let mut status = bindings::pbs_stathost(conn, 0 as *mut i8, 0 as *mut bindings::attrl, 0 as *mut i8);
-        while status != 0 as *mut bindings::batch_status {
-            println!("{}", ffi::CStr::from_ptr((*status).name).to_str().unwrap());
+        *bindings::pbs_stathost(conn, 0 as *mut i8, 0 as *mut bindings::attrl, 0 as *mut i8)
+    })
+}
 
-            let mut attribs = (*status).attribs;
-            while attribs != 0 as *mut bindings::attrl{
-                let r = (*attribs).resource;
-                let resource = {
-                    if r != 0 as *mut i8 {
-                        ffi::CStr::from_ptr(r).to_str().unwrap()
-                    }else{""}
-                };
 
-                println!("\t{}:{} {}",
-                    ffi::CStr::from_ptr((*attribs).name).to_str().unwrap(), 
-                    resource,
-                    ffi::CStr::from_ptr((*attribs).value).to_str().unwrap());
-                attribs = (*attribs).next;
-            }
-            println!("\n");
-            status = (*status).next;
-        }
+fn main() {
+    for node in get_nodes() {
+        println!("{}", unsafe {ffi::CStr::from_ptr(node.name).to_str().unwrap()});
+        for attrib in LinkedList::new(unsafe{*node.attribs}) {
+            let resource = unsafe {
+                if attrib.resource != 0 as *mut i8 {
+                    ffi::CStr::from_ptr(attrib.resource).to_str().unwrap()
+                }else{
+                    ""
+                }
+            };
+            let name = unsafe { ffi::CStr::from_ptr(attrib.name).to_str().unwrap() };
+            let value = unsafe { ffi::CStr::from_ptr(attrib.value).to_str().unwrap() };
+
+            println!("\t{}:{} {}", name, resource, value);
+       }
     }
 }
