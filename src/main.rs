f76c8adfbab43638e7ddb6b07773dc0f2b8d2112
Will Shanks <shanks@ucar.edu>
2022-08-18 13:38:48 -0600

    added questat, refactored

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,22 @@
-use std::ffi;
-use std::collections::HashMap;
-use serde_json::json;
-
 mod pbs;
-mod linked_list;
 
-use pbs::bindings;
-use linked_list::LinkedList;
-
-const METRICS: [&str; 3] =  ["state", "resources_available", "resources_assigned"];
-const RESOURCES: [&str; 3] = ["ncpus", "mem", "ngpus"];
-
-linked_list::impl_LlItem!{[bindings::attrl, bindings::batch_status]}
-
-fn get_nodes() -> LinkedList<bindings::batch_status> {
-    LinkedList::new( unsafe {
-        let conn = bindings::pbs_connect(0 as *mut i8);
-        // second arg is null to get all nodes, third is null to get all attributes, forth is unused
-        *bindings::pbs_stathost(conn, 0 as *mut i8, 0 as *mut bindings::attrl, 0 as *mut i8)
-    })
-}
-
-fn get_attribs(attrl: bindings::attrl) -> HashMap<String, String> {
-    let mut attribs: HashMap<String, String> = LinkedList::new(attrl)
-    .map(|attrib| {
-        let name = unsafe { ffi::CStr::from_ptr(attrib.name).to_str().unwrap() };
-        if METRICS.contains(&name) {
-        let resource = unsafe {
-            if attrib.resource != 0 as *mut i8 {
-                ffi::CStr::from_ptr(attrib.resource).to_str().unwrap()
-            }else{
-                ""
-            }
-        };
-        if RESOURCES.contains(&resource) || resource == "" {
-        let value = unsafe { ffi::CStr::from_ptr(attrib.value).to_str() }.unwrap().to_string();
-        let mut key = name.to_owned();
-
-        if resource != "" {
-            key.push_str("_");
-            key.push_str(resource);
-        }
-        Some((key, value))
-        }else{None}
-        }else{None}
-    })
-    .filter(|x| x.is_some())
-    .map(|x| x.unwrap())
-    .collect();
-
-    for resource in RESOURCES {
-        let mut k = "resources_available_".to_owned();
-        k.push_str(resource);
-        if !attribs.contains_key(&k) {
-            let mut drop_k = "resources_assigned_".to_owned();
-            drop_k.push_str(resource);
-            attribs.remove(&drop_k);
-        }
-    } 
-    return attribs;
-}
-
-
+use std::env;
 
 fn main() {
-    for node in get_nodes() {
-        let hostname = unsafe {ffi::CStr::from_ptr(node.name).to_str().unwrap()};
-        println!("{hostname}");
-        let mut attribs = get_attribs(unsafe{*node.attribs});
-        attribs.insert("hostname".to_string(), hostname.to_string());
-        println!("{}", json!(attribs));
+    let measurment;
+    let datapoints;
+    match env::args().nth(1).expect("Requires an arg, nodestat or questat").as_str() {
+    "nodestat" => {
+        datapoints = pbs::statnodes();
+        measurment = "pbs_nodestat";
+    },
+    "questat" => {
+        datapoints = pbs::statques();
+        measurment = "pbs_questat";
+    },
+    _ => panic!("requires an arg, nodestat or questat")
     }
+    print!("{{ \"measurement\": \"{}\", \"datapoints\": ", measurment);
+    print!("{}", serde_json::to_string(&datapoints).unwrap());
+    println!("}}");
 }
--- src/pbs.rs
+++ src/pbs.rs
@@ -1,1 +1,55 @@
-pub mod bindings;
+use std::collections::HashMap;
+use std::ptr::{null,NonNull};
+use std::ffi::CStr;
+
+mod bindings;
+mod linked_list;
+
+use linked_list::LinkedList;
+
+linked_list::impl_LlItem!{[bindings::attrl, bindings::batch_status]}
+
+fn parse_status(status: bindings::batch_status, name: &str) -> HashMap<String, String> {
+    let mut parsed = LinkedList::new(unsafe{*status.attribs})
+    .map(|attrib| {
+        let name = unsafe { CStr::from_ptr(attrib.name).to_str().unwrap() };
+        let resource = unsafe {
+            if let Some(_) = NonNull::new(attrib.resource) {
+                CStr::from_ptr(attrib.resource).to_str().unwrap()
+            }else{
+                ""
+            }
+        };
+        let value = unsafe { CStr::from_ptr(attrib.value).to_str() }.unwrap().to_string();
+        let mut key = name.to_owned();
+
+        if resource != "" {
+            key.push_str("_");
+            key.push_str(resource);
+        }
+        (key, value)
+    })
+    .collect::<HashMap<String,String>>();
+    parsed.insert(name.to_string(), unsafe{CStr::from_ptr(status.name).to_str()}.unwrap().to_string());
+    return parsed;
+}
+
+fn stat_pbs(f: &dyn Fn(i32) -> bindings::batch_status, name: &str) -> Vec<HashMap<String,String>> {
+    let conn = unsafe{bindings::pbs_connect(null::<i8>() as *mut i8)};
+    let resp = LinkedList::new(f(conn));
+    unsafe{bindings::pbs_disconnect(conn)};
+
+    //make sure to insert resource name into metric
+    resp.map(|x| parse_status(x, name)).collect()
+} 
+    
+
+pub fn statnodes() -> Vec<HashMap<String, String>> {
+    // second arg is null to get all nodes, third is null to get all attributes, forth is unused
+    stat_pbs( &|conn| unsafe {*bindings::pbs_stathost(conn, null::<i8>() as *mut i8, null::<bindings::attrl>() as *mut bindings::attrl, null::<i8>() as *mut i8)}, "hostname")
+}
+
+pub fn statques() -> Vec<HashMap<String, String>> {
+    stat_pbs( &|conn| unsafe {*bindings::pbs_statque(conn, null::<i8>() as *mut i8, null::<bindings::attrl>() as *mut bindings::attrl, null::<i8> as *mut i8)}, "que")
+}
+
